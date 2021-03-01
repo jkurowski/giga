08812a4ea1f19aa713070d7e92e3b67ebb02fbf5
Jacek <kontakt@jk-design.pl>
2021-03-01 02:11:08 +0100

    Init

---
+++ application/modules/default/controllers/CovidController.php
@@ -0,0 +1,45 @@
+<?php
+
+class Default_CovidController extends kCMS_Site
+{
+    /**
+     * @var string
+     */
+    private $page_class;
+    /**
+     * @var int
+     */
+    private $page_id;
+
+    public function preDispatch() {
+        $this->page_id = 6;
+        $this->page_class = '';
+    }
+
+    public function indexAction() {
+        $this->_helper->layout->setLayout('page');
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_Db::FETCH_OBJ);
+
+        $page = $db->fetchRow($db->select()->from('strony')->where('id = ?', $this->page_id));
+
+        if(!$page) {
+            errorPage();
+        } else {
+
+            $array = array(
+                'strona_nazwa' => $page->nazwa,
+                'strona_h1' => $page->nazwa,
+                'strona_tytul' => ' - '.$page->nazwa,
+                'seo_tytul' => $page->meta_tytul,
+                'seo_opis' => $page->meta_opis,
+                'seo_slowa' => $page->meta_slowa,
+                'strona_id' => $this->page_id,
+                'strona' => $page,
+                'pageclass' => $this->page_class
+            );
+            $this->view->assign($array);
+
+        }
+    }
+}
