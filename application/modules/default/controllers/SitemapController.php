a611a9143ca564f62925ce10e9ba7b188d1d6f43
Jacek <kontakt@jk-design.pl>
2021-08-06 16:55:30 +0200

    Podmiana logo

---
+++ application/modules/default/controllers/SitemapController.php
@@ -0,0 +1,39 @@
+<?php
+class Default_SitemapController extends kCMS_Site
+{
+
+    private $page_class;
+    private $page_id;
+
+    public function preDispatch() {
+        $this->page_id = 12;
+        $this->page_class = '';
+    }
+
+    public function indexAction() {
+        $this->_helper->layout->setLayout('page');
+        $db = Zend_Registry::get('db');
+        $db->setFetchMode(Zend_Db::FETCH_OBJ);
+
+        $page = $db->fetchRow($db->select()->from('strony')->where('id = ?', $this->page_id));
+
+        if(!$page) {
+            errorPage();
+        } else {
+
+            $array = array(
+                'strona_nazwa' => $page->nazwa,
+                'strona_h1' => $page->nazwa,
+                'strona_tytul' => ' - '.$page->nazwa,
+                'seo_tytul' => $page->meta_tytul,
+                'seo_opis' => $page->meta_opis,
+                'seo_slowa' => $page->meta_slowa,
+                'strona_id' => $this->page_id,
+                'strona' => $page,
+                'pageclass' => $this->page_class
+            );
+            $this->view->assign($array);
+
+        }
+    }
+}
